92051dfb1a40e208138776320188206f7b81254b
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-03-22 00:28:30 +0200

    fixed date schema

--- src/schemas/bookVanSchema.js
+++ src/schemas/bookVanSchema.js
@@ -12,9 +12,7 @@ const bookVanSchema = Yup.object().shape({
       "Invalid email format"
     )
     .required("Email is required"),
-  date: Yup.date()
-    .min(new Date(), "Date must be grater than now")
-    .required("Date is required"),
+  date: Yup.date().required("Date is required"),
   comment: Yup.string(),
 });
 
